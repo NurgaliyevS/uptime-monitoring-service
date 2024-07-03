83bc552143196e42fbaee7c00e3baf6e6d6e5324
NurgaliyevS <nurgaliyev000@gmail.com>
2024-07-03 21:15:38 +0500

    upd

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="keywords"
+          content="uptime web hosting, monitoring saas, server monitoring saas, monitoring saas applications, uptime robot alternatives"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />

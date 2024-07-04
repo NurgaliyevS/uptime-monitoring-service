f79533d496ec4b4170895ddebf95c56f87ca2de3
NurgaliyevS <nurgaliyev000@gmail.com>
2024-07-04 15:22:24 +0500

    upd

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
-import { Inter } from "next/font/google";
+import { Lato } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 export default function Home() {
   return (
-    <main
-      className={`flex flex-col min-h-screen mx-auto ${inter.className}`}
-    >sad</main>
+    <main className={`flex flex-col min-h-screen mx-auto ${lato.className}`}>
+      <h1 className="text-4xl font-bold text-center">
+        Uptime Friend is a simple uptime monitoring service
+      </h1>
+    </main>
   );
 }

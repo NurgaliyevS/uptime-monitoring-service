de4fe8013700bb9bdbbc2074547ed41d583faa00
NurgaliyevS <nurgaliyev000@gmail.com>
2024-07-06 18:00:25 +0500

    upd

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function Main() {
   return (
-    <section className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center lg:items-start justify-between px-8 py-8 lg:py-20 gap-16 lg:gap-20">
+    <section className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center lg:items-start justify-between px-8 py-10 lg:py-20 gap-16 lg:gap-20">
       <div className="flex flex-col gap-10 lg:gap-14 items-center lg:items-start text-center lg:text-left w-full lg:w-2/3">
         <h1 className="font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4 flex justify-center items-center f-w-">
           <span className="whitespace-nowrap relative">Uptime Web Hosting</span>

edef178b2278d8753db06758ee1452706af31e26
NurgaliyevS <nurgaliyev000@gmail.com>
2024-07-12 11:33:57 +0500

    upd

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,45 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { customConfig } from "@/project.custom.config";
+import clientPromise from "@/backend/mongodbClient";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+
+export const authOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      async profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.given_name ? profile.given_name : profile.name,
+          email: profile.email,
+          image: profile.picture,
+          createdAt: new Date(),
+        };
+      },
+    }),
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+
+  callbacks: {
+    session: async ({ session, token }) => {
+      if (session?.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
+  theme: {
+    brandColor: customConfig.dataTheme,
+    // Add you own logo below. Recommended size is rectangle (i.e. 200x50px) and show your logo + name.
+    // It will be used in the login flow to display your logo. If you don't add it, it will look faded.
+    logo: `https://uptimefriend.com/logoAndName200x50.png`,
+  },
+};
+
+export default NextAuth(authOptions);

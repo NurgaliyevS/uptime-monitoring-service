0c1d131092ed0235cbf2b62f62bed85c0278fd1b
NurgaliyevS <nurgaliyev000@gmail.com>
2024-07-12 12:45:12 +0500

    upd

--- backend/mongodbClient.js
+++ backend/mongodbClient.js
@@ -1,20 +1,28 @@
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient(process.env.MONGODB_URI, {
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+  throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
+}
+
+const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+};
 
+let client;
 let clientPromise;
 
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so the MongoClient is not constantly re-initialized
   if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable
+  client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 

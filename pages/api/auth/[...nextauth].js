55d13fd727fc2115f79ff3f1fff4df0a850dc2eb
NurgaliyevS <nurgaliyev000@gmail.com>
2024-07-12 13:02:10 +0500

    upd

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -35,7 +35,7 @@ export const authOptions = {
     strategy: "jwt",
   },
   theme: {
-    brandColor: customConfig.dataTheme,
+    // brandColor: customConfig.dataTheme,
     // Add you own logo below. Recommended size is rectangle (i.e. 200x50px) and show your logo + name.
     // It will be used in the login flow to display your logo. If you don't add it, it will look faded.
     logo: `https://uptimefriend.com/logoAndName200x50.png`,

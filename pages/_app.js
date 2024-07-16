f4489d5ad71b013aaac58b89f01626235174a714
NurgaliyevS <nurgaliyev000@gmail.com>
2024-07-16 16:10:49 +0500

    upd

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import { ToastContainer } from "react-toastify";
 
-export default function App({ Component, pageProps: { session, ...pageProps } }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </SessionProvider>
   );
 }

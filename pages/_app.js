5a7380b5db0f9ef09aab8ad01dc409ff02abb33b
NurgaliyevS <nurgaliyev000@gmail.com>
2024-07-16 16:22:46 +0500

    upd

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({
   Component,

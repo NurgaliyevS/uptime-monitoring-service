e5a27f24a7ed23fc70cf72f3e489ebede42b02fc
NurgaliyevS <nurgaliyev000@gmail.com>
2024-07-23 19:30:59 +0500

    upd

--- pages/api/cron/[id]/check.js
+++ pages/api/cron/[id]/check.js
@@ -3,23 +3,46 @@ import Monitor from "@/backend/monitor";
 import { checkMonitor } from "@/utils/monitoringService";
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const { id } = req.query;
+  await connectMongoDB();
 
-    await connectMongoDB();
+  if (req.method === "GET") {
+    const { id } = req.query;
 
     try {
       const monitor = await Monitor.findById(id);
-      console.log(monitor, 'monitor check in cron by id')
       if (!monitor) {
-        return res.status(404).json({ success: false, message: "Monitor not found" });
+        return res
+          .status(404)
+          .json({ success: false, message: "Monitor not found" });
+      }
+      await checkMonitor(monitor);
+      res.status(200).json({ success: true, message: "Check completed" });
+    } catch (error) {
+      console.error(`Error getting monitor ${id}:`, error);
+      res
+        .status(500)
+        .json({ success: false, message: "Error getting monitor" });
+    }
+  }
+  if (req.method === "POST") {
+    const { id } = req.query;
+
+    try {
+      const monitor = await Monitor.findById(id);
+      console.log(monitor, "monitor check in cron by id");
+      if (!monitor) {
+        return res
+          .status(404)
+          .json({ success: false, message: "Monitor not found" });
       }
 
       await checkMonitor(monitor);
       res.status(200).json({ success: true, message: "Check completed" });
     } catch (error) {
       console.error(`Error checking monitor ${id}:`, error);
-      res.status(500).json({ success: false, message: "Error checking monitor" });
+      res
+        .status(500)
+        .json({ success: false, message: "Error checking monitor" });
     }
   } else {
     res.setHeader("Allow", ["POST"]);

3fc1e40c2a83d61af18b75f039fa2ee3f6119e71
NurgaliyevS <nurgaliyev000@gmail.com>
2024-07-25 12:29:38 +0500

    upd

--- pages/api/cron/[id]/check.js
+++ pages/api/cron/[id]/check.js
@@ -4,26 +4,6 @@ import { checkMonitor } from "@/utils/monitoringService";
 
 export default async function handler(req, res) {
   await connectMongoDB();
-
-  // if (req.method === "GET") {
-  //   const { id } = req.query;
-
-  //   try {
-  //     const monitor = await Monitor.findById(id);
-  //     if (!monitor) {
-  //       return res
-  //         .status(404)
-  //         .json({ success: false, message: "Monitor not found" });
-  //     }
-  //     await checkMonitor(monitor);
-  //     res.status(200).json({ success: true, message: "Check completed" });
-  //   } catch (error) {
-  //     console.error(`Error getting monitor ${id}:`, error);
-  //     res
-  //       .status(500)
-  //       .json({ success: false, message: "Error getting monitor" });
-  //   }
-  // }
   if (req.method === "POST") {
     const { id } = req.query;
 

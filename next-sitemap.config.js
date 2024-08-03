8224ebb85fda67d252b9323b84d67732dad462f8
NurgaliyevS <nurgaliyev000@gmail.com>
2024-08-03 13:47:41 +0500

    upd

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: "https://uptimefriend.com",
-    generateRobotsTxt: true,
+  siteUrl: "https://uptimefriend.com",
+  generateRobotsTxt: true,
+  exclude: ["/admin", "/blocked"],
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      "https://uptimefriend.com/sitemap.xml",
+    ],
+  },
 };
